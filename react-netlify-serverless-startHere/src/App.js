fd36a47ff3c3199ede57d769b2e6ca050575cc84
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2021-01-20 17:35:59 +0100

    all

---
+++ react-netlify-serverless-startHere/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Wrapper, Form } from './App.styles.js';
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isSent, setIsSent] = useState(false);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper>
+      {isSent ? (
+        <div>Message sent.</div>
+      ) : (
+        <Form onSubmit={handleSubmit}>
+          <TextField
+            value={name}
+            onChange={e => setName(e.currentTarget.value)}
+            label='Name'
+            variant='outlined'
+          />
+          <TextField
+            value={email}
+            type='email'
+            onChange={e => setEmail(e.currentTarget.value)}
+            label='E-mail'
+            variant='outlined'
+          />
+          <Button type='submit' variant='contained' color='primary'>
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Wrapper>
+  );
+};
+
+export default App;
---
+++ react-netlify-serverless-startHere/src/App.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 400px;
+  height: 200px; 
+  padding: 40px;
+  border: 1px solid lightblue;
+  border-radius: 10px;
+`;
